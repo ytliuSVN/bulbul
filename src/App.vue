473d827da2c319e88618245cc99e707a4dd817e9
Penny <enderman542@gmail.com>
2025-05-14 17:49:00 +0800

    fine-tune css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="app">
-    <!-- Mobile only content -->
-    <div class="is-mobile">
-      <the-navigation class="nav-section"></the-navigation>
-    </div>
-
     <!-- Desktop only content -->
-    <!-- <div class="is-desktop">side bar</div> -->
+    <div class="is-desktop">
+      <the-sidebar-menu />
+    </div>
 
     <!-- Content that appears in both views -->
     <main>
+      <!-- Mobile only content -->
+      <div class="is-mobile">
+        <the-navigation />
+      </div>
+
       <the-banner>
         <template #title>{{ TEXT_CONSTANTS.BANNER.TITLE }}</template>
         <template #subtitle>{{ TEXT_CONSTANTS.BANNER.SUBTITLE }}</template>
@@ -21,8 +23,25 @@
 
 <script setup>
 import TheNavigation from "./components/nav/TheNavigation.vue";
+import TheSidebarMenu from "./components/nav/TheSidebarMenu.vue";
 import TheBanner from "./components/banner/TheBanner.vue";
 import { TEXT_CONSTANTS } from "@/utility/textConstants";
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    width: 100%;
+    overflow-x: hidden;
+
+    @media (max-width: 1024px) {
+      padding-left: 0;
+      margin: 0;
+      min-width: 0;
+    }
+  }
+}
+</style>
