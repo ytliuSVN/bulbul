f32a8574cf468800ebe2bed69b49a2607db43588
Penny <enderman542@gmail.com>
2025-06-03 23:26:39 +0800

    resolve the dynamic import issues

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bulbul/', // Set the base path for the app
+  base: process.env.NODE_ENV === 'production' ? '/bulbul/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
